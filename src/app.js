4e68e24c28cc026e701b4301ae01fbde1c6f0499
marco aponte <marcoaponteleon.com>
2023-11-20 19:39:21 -0300

    primer commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,31 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+const container = document.querySelector(".container");
+const pintaSuperior = document.querySelector(".pintaSuperior");
+const pintaInferior = document.querySelector(".pintaInferior");
+const numero = document.querySelector(".numero");
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pintas = ["♦", "♥", "♠", "♣"];
+  let numeros = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let pintasRandon = Math.floor(Math.random() * pintas.length);
+  let numerosRandon = Math.floor(Math.random() * numeros.length);
+  pintaSuperior.innerHTML = pintas[pintasRandon];
+  pintaInferior.innerHTML = pintas[pintasRandon];
+  numero.innerHTML = numeros[numerosRandon];
 };
